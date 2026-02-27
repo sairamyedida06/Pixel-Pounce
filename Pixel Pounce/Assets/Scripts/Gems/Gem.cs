df49165121bcb5d8e25d7f0867b11c967d38fcc4
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-02-27 13:18:24 +0530

    feat (gem): add collectable gem

---
+++ Pixel Pounce/Assets/Scripts/Gems/Gem.cs
@@ -0,0 +1,13 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
